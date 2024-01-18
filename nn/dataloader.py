407632a0b6cc56ba2b13f6607056c3964efbf3b6
Sébastien GUILLEMIN <sebastien.guillemin11@gmail.com>
2024-01-18 16:26:03 +0100

    Maj

--- nn/dataloader.py
+++ nn/dataloader.py
@@ -6,10 +6,11 @@ from samples_dataset import SamplesDataset
 from torch.utils.data import DataLoader
 from torch.utils.data import random_split
 from tqdm.notebook import tqdm
+import csv
 
 quanti_cols_df_names = ['id', '_3_4_methylenedioxyethylamphetamine', '_3_4_methylene_dioxy_methylamphetamine', '_3_4_methylenedioxyphenylacetone', '_4f_mdmb_butinaca', '_5f_adb', '_5f_mdmb_pica', 'acetylcodeine', 'acetylthebaol', 'acide_4_hydroxybutanoique', 'acide_benzoique', 'acide_borique', 'adb_butinaca', 'amidon', 'aminopyrine', 'amoxycilline', 'amphetamine', 'benzocaine', 'benzoylecgonine', 'bicarbonate_de_sodium', 'bicarbonates', 'buprenorphine', 'cafeine', 'cannabidiol', 'cannabigerol', 'cannabinol', 'cis_cinnamoylcocaine', 'clonazepam', 'clotrimazole', 'cocaine', 'codeine', 'creatine', 'delta8_tetrahydrocannabinol', 'delta9_tetrahydrocannabinol', 'dextromethorphane', 'diametre', 'diltiazem', 'dimethylterephtalate', 'diphenydramine', 'ecgonidine', 'ecgoninemethylester', 'epaisseur', 'fub_amb', 'gammabutyrolactone', 'glucose', 'griseofulvine', 'guaifenesine', 'hauteur', 'heroine', 'hydroxyzine', 'ibuprofene', 'inositol', 'isoleucine', 'ketamine', 'lactitol', 'lactose', 'largeur', 'leucine', 'levamisole', 'lidocaine', 'longueur', 'maltose', 'mannitol', 'masse', 'mdmb_4en_pinaca', 'meconine', 'methamphetamine', 'methylecgonidine', 'monoacetylmorphine', 'morphine', 'nicotine', 'n_methyl_tryptamine', 'norcocaine', 'noscapine', 'o3_monoacetylmorphine_', 'o6_monoacetylmorphine', 'oxycodone', 'papaverine', 'paracetamol', 'phenacetine', 'piracetam', 'procaine', 'pv8', 'saccharose', 'sorbitol', 'talc', 'taux_cbd', 'taux_cbn', 'terephtalates', 'tetracaine', 'tramadol', 'trans_cinnamoylcocaine', 'tropacocaine', 'uree', 'valine', 'vanilline', 'id_lot']
 
-quali_cols_df_names = ['abime', 'cbd', 'cbn', 'couleur', 'couleur_exterieur', 'couleur_exterieur_1', 'couleur_exterieur_2', 'couleur_interieur', 'description_de_l_objet', 'etiquette', 'forme', 'logo', 'nom_de_logo', 'numero_echantillon', 'ovule', 'presentation', 'secabilite_recto', 'secabilite_verso', 'type_drogue', 'visqueux']
+quali_cols_df_names = ['abime', 'cbd', 'cbn', 'couleur', 'couleur_exterieur', 'couleur_exterieur_1', 'couleur_exterieur_2', 'couleur_interieur', 'etiquette', 'forme', 'logo', 'ovule', 'presentation', 'secabilite_recto', 'secabilite_verso', 'type_drogue', 'visqueux']
 
 def clean_samples(samples):
     empty_cols = samples.columns[(samples == 0).all()]
@@ -18,6 +19,30 @@ def clean_samples(samples):
     print(f'Cleaning samples columns : {len(empty_cols) + 2} columns dropped.')
 
     return clean_samples
+
+def process_quantiative_data(samples, normalize=True):
+    # Quantitative columns
+    quanti = samples[quanti_cols_df_names].astype(float)
+    
+    # ----Normalize if needed
+    if normalize:
+        max_quanti = quanti.max()
+        quanti = quanti.divide(max_quanti)
+
+    quanti = quanti.fillna(value=0.0)
+    # quanti = clean_samples(samples=quanti)
+    
+    return quanti
+
+def process_qualitative_data(samples):
+    # Qualitative columns
+    quali = samples[quali_cols_df_names]
+    quali = quali.replace(to_replace='Oui', value='1')
+    quali = quali.replace(to_replace='Non', value='0')
+    quali = pd.get_dummies(quali, drop_first=True, dtype=float)
+    quali = quali.replace(to_replace=1, value=0.5)
+    
+    return quali
 
 def load_samples(positive_examples_count=-1, normalize=True, drug_type=None):
     # Loading data
@@ -48,34 +73,27 @@ def load_samples(positive_examples_count=-1, normalize=True, drug_type=None):
     colnames = [desc[0] for desc in cur.description]
 
     samples = pd.DataFrame(list(samples), columns=colnames)
-    samples.drop(columns=['description_de_l_objet', 'nom_de_logo', 'numero_echantillon', ])
-
-    # Quantitative columns
-    quanti_cols = samples[quanti_cols_df_names].astype(float)
     
-    # ----Normalize if needed
-    if normalize:
-        max_quanti_cols = quanti_cols.max()
-        quanti_cols = quanti_cols.divide(max_quanti_cols)
-
-    quanti_cols = quanti_cols.fillna(value=0.0)
-    quanti_cols = clean_samples(samples=quanti_cols)
-
-    # Qualitative columns
-    quali_cols = samples[quali_cols_df_names]
-    quali_cols = quali_cols.replace(to_replace='Oui', value='1')
-    quali_cols = quali_cols.replace(to_replace='Non', value='0')
-    quali_cols = pd.get_dummies(quali_cols, drop_first=True, dtype=float)
+    quanti = process_quantiative_data(samples=samples, normalize=normalize)
+    quali = process_qualitative_data(samples=samples)
     
     # Merge quantitative and qualitative
-    samples = pd.merge(quali_cols, quanti_cols, left_index=True, right_index=True)
+    samples = pd.merge(quali, quanti, left_index=True, right_index=True)
+    print(samples.shape)
+    
+    columns_names_1 = list(samples.columns)
+    columns_names_1.remove('id_lot')
+    columns_names_2 = list(map(lambda name: name + '_ech2', columns_names_1))
 
-
+    columns_names = columns_names_1 + columns_names_2
+    print(columns_names)
+    print(len(columns_names))
+    
     # Subsampling data if needed
     if positive_examples_count != -1:
         samples = samples.iloc[:positive_examples_count, :]
 
-    return samples
+    return samples, columns_names
 
 def create_positive_examples(samples):
     print('Creating positive examples.')
@@ -98,6 +116,7 @@ def create_positive_examples(samples):
                 if i != j:
                     sample2 = batch_samples[j]
                     dataset_tuples.append((sample1, sample2, 1))
+                    
     return dataset_tuples
 
 def create_negative_examples(dataset_tuples, replication_factor=2):
@@ -120,12 +139,23 @@ def create_negative_examples(dataset_tuples, replication_factor=2):
     dataset_tuples += counter_examples
     return dataset_tuples
     
-def load_samples_dataset(positive_examples_count=-1, drug_type=None):
-    samples = load_samples(positive_examples_count=positive_examples_count, drug_type=drug_type)
+def load_samples_dataset(positive_examples_count=-1, drug_type=None, export_as_csv=False, from_csv=False):
+    dataset = None
+    if not from_csv:
+        samples, columns_names = load_samples(positive_examples_count=positive_examples_count, drug_type=drug_type)
 
-    dataset_tuples = create_positive_examples(samples=samples)
-    dataset_tuples = create_negative_examples(dataset_tuples=dataset_tuples)    
+        dataset_tuples = create_positive_examples(samples=samples)
+        dataset_tuples = create_negative_examples(dataset_tuples=dataset_tuples)
+        
+        dataset = SamplesDataset(dataset_tuples, columns_names=columns_names)
+        
+        if export_as_csv:
+            dataset.export_as_csv(path='../data/dataset.csv')
+    else:
+        dataset = SamplesDataset()
+        dataset.load_csv(path='../data/dataset.csv')
 
-    print(f'Dataset size : {len(dataset_tuples)}')
-
-    return SamplesDataset(dataset_tuples)
+    print(f'Dataset size : {len(dataset)}')
+        
+    return dataset
+    
--- nn/samples_dataset.py
+++ nn/samples_dataset.py
@@ -1,11 +1,12 @@
 import numpy as np
 from torch.utils.data import Dataset
 import torch
-import random
+import pandas as pd 
 
 
 class SamplesDataset(Dataset):
-    def __init__(self, tuples=[]):
+    def __init__(self, tuples=[], columns_names=[]):
+        self.columns_names = columns_names
         self.pairs = []
         self.labels = []
 
@@ -17,8 +18,12 @@ class SamplesDataset(Dataset):
         return len(self.pairs)
 
     def __getitem__(self, index):
-        X, y = torch.tensor(self.pairs[index], dtype=torch.float32), torch.tensor(self.labels[index], dtype=torch.float32)
+        X = torch.tensor(self.pairs[index], dtype=torch.float32)
+        y = torch.tensor(self.labels[index], dtype=torch.float32)
         return X, y
+    
+    def get_columns_names(self):
+        return self.columns_names
     
     def add_examples(self, examples, labels):
         if len(examples) != len(labels):
@@ -36,3 +41,24 @@ class SamplesDataset(Dataset):
     
     def get_row_dataset(self):
         return self.pairs, self.labels
+    
+    def export_as_csv(self, path):
+        X_as_array = np.asarray(self.pairs)
+        y_as_array = np.asarray([self.labels]).T
+        concat = np.concatenate((X_as_array, y_as_array), axis=1)
+        
+        df_columns_names = self.columns_names + ['label']
+        
+        df = pd.DataFrame(concat, columns=df_columns_names)
+        df.to_csv(path, index=False)
+        
+    def load_csv(self, path):
+        df = pd.read_csv(path)
+        df_columns_count = len(df.columns)
+        
+        labels_df = df.iloc[:, df_columns_count - 1]
+        self.labels = labels_df.values.tolist()
+        
+        pairs_df = df.drop(df.columns[df_columns_count - 1], axis=1)
+        self.pairs = pairs_df.to_numpy()       
+        
