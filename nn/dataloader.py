745d91bf1a6325c559c6e3ed570bb197cc6cb53c
Sébastien GUILLEMIN <sebastien.guillemin11@gmail.com>
2024-02-06 18:29:28 +0100

    Changement dataloading

--- nn/dataloader.py
+++ nn/dataloader.py
@@ -1,16 +1,14 @@
-import psycopg2
 import pandas as pd
-import dataloader as dl
 import random
 from samples_dataset import SamplesDataset
-from torch.utils.data import DataLoader
-from torch.utils.data import random_split
 from tqdm.notebook import tqdm
-import csv
+import os.path
 
-quanti_cols_df_names = ['id', '_3_4_methylenedioxyethylamphetamine', '_3_4_methylene_dioxy_methylamphetamine', '_3_4_methylenedioxyphenylacetone', '_4f_mdmb_butinaca', '_5f_adb', '_5f_mdmb_pica', 'acetylcodeine', 'acetylthebaol', 'acide_4_hydroxybutanoique', 'acide_benzoique', 'acide_borique', 'adb_butinaca', 'amidon', 'aminopyrine', 'amoxycilline', 'amphetamine', 'benzocaine', 'benzoylecgonine', 'bicarbonate_de_sodium', 'bicarbonates', 'buprenorphine', 'cafeine', 'cannabidiol', 'cannabigerol', 'cannabinol', 'cis_cinnamoylcocaine', 'clonazepam', 'clotrimazole', 'cocaine', 'codeine', 'creatine', 'delta8_tetrahydrocannabinol', 'delta9_tetrahydrocannabinol', 'dextromethorphane', 'diametre', 'diltiazem', 'dimethylterephtalate', 'diphenydramine', 'ecgonidine', 'ecgoninemethylester', 'epaisseur', 'fub_amb', 'gammabutyrolactone', 'glucose', 'griseofulvine', 'guaifenesine', 'hauteur', 'heroine', 'hydroxyzine', 'ibuprofene', 'inositol', 'isoleucine', 'ketamine', 'lactitol', 'lactose', 'largeur', 'leucine', 'levamisole', 'lidocaine', 'longueur', 'maltose', 'mannitol', 'masse', 'mdmb_4en_pinaca', 'meconine', 'methamphetamine', 'methylecgonidine', 'monoacetylmorphine', 'morphine', 'nicotine', 'n_methyl_tryptamine', 'norcocaine', 'noscapine', 'o3_monoacetylmorphine_', 'o6_monoacetylmorphine', 'oxycodone', 'papaverine', 'paracetamol', 'phenacetine', 'piracetam', 'procaine', 'pv8', 'saccharose', 'sorbitol', 'talc', 'taux_cbd', 'taux_cbn', 'terephtalates', 'tetracaine', 'tramadol', 'trans_cinnamoylcocaine', 'tropacocaine', 'uree', 'valine', 'vanilline', 'id_lot']
-
+quanti_cols_df_names = ['id', '_3_4_methylenedioxyethylamphetamine', '_3_4_methylene_dioxy_methylamphetamine', '_3_4_methylenedioxyphenylacetone', '_4f_mdmb_butinaca', '_5f_adb', '_5f_mdmb_pica', 'acetylcodeine', 'acetylthebaol', 'acide_4_hydroxybutanoique', 'acide_benzoique', 'acide_borique', 'adb_butinaca', 'amidon', 'aminopyrine', 'amoxycilline', 'amphetamine', 'benzocaine', 'benzoylecgonine', 'bicarbonate_de_sodium', 'bicarbonates', 'buprenorphine', 'cafeine', 'cannabidiol', 'cannabigerol', 'cannabinol', 'cis_cinnamoylcocaine', 'clonazepam', 'clotrimazole', 'cocaine', 'codeine', 'creatine', 'delta8_tetrahydrocannabinol', 'delta9_tetrahydrocannabinol', 'dextromethorphane', 'diametre', 'diltiazem', 'dimethylterephtalate', 'diphenydramine', 'ecgonidine', 'ecgoninemethylester', 'epaisseur', 'fub_amb', 'gammabutyrolactone', 'glucose', 'griseofulvine', 'guaifenesine', 'hauteur', 'heroine', 'hydroxyzine', 'ibuprofene', 'inositol', 'isoleucine', 'ketamine', 'lactitol', 'lactose', 'largeur', 'leucine', 'levamisole', 'lidocaine', 'longueur', 'maltose', 'mannitol', 'masse', 'mdmb_4en_pinaca', 'meconine', 'methamphetamine', 'methylecgonidine', 'monoacetylmorphine', 'morphine', 'nicotine', 'n_methyl_tryptamine', 'norcocaine', 'noscapine', 'o3_monoacetylmorphine', 'o6_monoacetylmorphine', 'oxycodone', 'papaverine', 'paracetamol', 'phenacetine', 'piracetam', 'procaine', 'pv8', 'saccharose', 'sorbitol', 'talc', 'taux_cbd', 'taux_cbn', 'terephtalates', 'tetracaine', 'tramadol', 'trans_cinnamoylcocaine', 'tropacocaine', 'uree', 'valine', 'vanilline', 'id_lot']
 quali_cols_df_names = ['abime', 'cbd', 'cbn', 'couleur', 'couleur_exterieur', 'couleur_exterieur_1', 'couleur_exterieur_2', 'couleur_interieur', 'etiquette', 'forme', 'logo', 'ovule', 'presentation', 'secabilite_recto', 'secabilite_verso', 'type_drogue', 'visqueux']
+
+samples_csv_data_path = '../data/clustering/dataset.csv'
+nn_csv_data_path = '../data/nn/dataset.csv'
 
 def clean_samples(samples):
     empty_cols = samples.columns[(samples == 0).all()]
@@ -20,7 +18,7 @@ def clean_samples(samples):
 
     return clean_samples
 
-def process_quantiative_data(samples, normalize: bool =True):
+def process_quantiative_data(samples, normalize: bool =True) -> pd.DataFrame:
     # Quantitative columns
     quanti = samples[quanti_cols_df_names].astype(float)
     
@@ -30,67 +28,18 @@ def process_quantiative_data(samples, normalize: bool =True):
         quanti = quanti.divide(max_quanti)
 
     quanti = quanti.fillna(value=0.0)
-    # quanti = clean_samples(samples=quanti)
     
     return quanti
 
-def process_qualitative_data(samples):
+def process_qualitative_data(samples: pd.DataFrame) -> pd.DataFrame:
     # Qualitative columns
     quali = samples[quali_cols_df_names]
     quali = quali.replace(to_replace='Oui', value='1')
-    quali = quali.replace(to_replace='Non', value='0')
+    quali = quali.replace(to_replace='Non', value='0')  
     quali = pd.get_dummies(quali, drop_first=True, dtype=float)
     quali = quali.replace(to_replace=1, value=0.5)
     
     return quali
-
-def load_samples(positive_examples_count: int =-1, normalize: bool =True, drug_type: str =None) -> tuple[pd.DataFrame, list[str]] :
-    # Loading data
-    conn = psycopg2.connect(database="full_STUPS",
-                            user="postgres",
-                            host='localhost',
-                            password="postgres",
-                            port=5432)
-    cur = conn.cursor()
-
-    query =  '''select distinct ep.*, id_lot
-                from echantillon_propriete ep
-                    inner join echantillon e on e.id = ep.id
-                    inner join composition c on c.id = e.id_composition
-                    inner join lot_complet lc on (
-                        lc.e1 = c.id
-                        or lc.e2 = c.id
-                    )'''
-    
-    if drug_type != None:
-        query += f'WHERE type_drogue = \'{drug_type}\''
-
-    cur.execute(query)
-
-    samples = cur.fetchall()
-    conn.commit()
-    conn.close()
-    colnames = [desc[0] for desc in cur.description]
-
-    samples = pd.DataFrame(list(samples), columns=colnames)
-    
-    quanti = process_quantiative_data(samples=samples, normalize=normalize)
-    quali = process_qualitative_data(samples=samples)
-    
-    # Merge quantitative and qualitative
-    samples = pd.merge(quali, quanti, left_index=True, right_index=True)
-    
-    columns_names_1 = list(samples.columns)
-    columns_names_1.remove('id_lot')
-    columns_names_2 = list(map(lambda name: name + '_ech2', columns_names_1))
-
-    columns_names = columns_names_1 + columns_names_2
-    
-    # Subsampling data if needed
-    if positive_examples_count != -1:
-        samples = samples.iloc[:positive_examples_count, :]
-
-    return samples, columns_names
 
 def create_positive_examples(samples: pd.DataFrame) -> list[tuple]:
     print('Creating positive examples.')
@@ -135,23 +84,44 @@ def create_negative_examples(dataset_tuples: list[tuple], replication_factor: in
     dataset_tuples += counter_examples
     return dataset_tuples
     
-def load_samples_dataset(positive_examples_count=-1, drug_type=None, export_as_csv=False, from_csv=False) -> SamplesDataset:
-    dataset = None
-    if not from_csv:
-        samples, columns_names = load_samples(positive_examples_count=positive_examples_count, drug_type=drug_type)
+def load_samples_dataset(positive_examples_count=-1, drug_type=None, from_csv=True, normalize=True) -> SamplesDataset:
+    dataset = SamplesDataset()
+    
+    if from_csv and os.path.isfile(nn_csv_data_path):
+        dataset.load_csv(path=nn_csv_data_path)
 
-        dataset_tuples = create_positive_examples(samples=samples)
-        dataset_tuples = create_negative_examples(dataset_tuples=dataset_tuples)
-        
-        dataset = SamplesDataset(dataset_tuples, columns_names=columns_names)
-        
-        if export_as_csv:
-            dataset.export_as_csv(path='../data/dataset.csv')
     else:
-        dataset = SamplesDataset()
-        dataset.load_csv(path='../data/dataset.csv')
+        if not os.path.isfile(samples_csv_data_path):
+            raise Exception(f'Samples file {samples_csv_data_path} does not exists.')
+        else:
+            samples = pd.read_csv(samples_csv_data_path)
+            
+            if positive_examples_count != -1:
+                samples = samples.iloc[:positive_examples_count, :]
+                
+            if drug_type != None:
+                samples = samples[samples['type_drogue'] == drug_type]
 
+            print(f'Samples shape : {samples.shape}.\n')
+            
+            quali = process_qualitative_data(samples=samples)
+            quanti = process_quantiative_data(samples=samples, normalize=normalize)
+            samples = pd.merge(quali, quanti, left_index=True, right_index=True)
+            
+            samples = clean_samples(samples)
+            print(f'Samples shape after clean and dummy variables : {samples.shape}.\n')
+            
+            columns_names_1 = list(samples.columns)
+            columns_names_1.remove('id_lot')
+            columns_names_2 = list(map(lambda name: name + '_ech2', columns_names_1))
+            columns_names = columns_names_1 + columns_names_2
+                
+            dataset_tuples = create_positive_examples(samples=samples)
+            dataset_tuples = create_negative_examples(dataset_tuples=dataset_tuples)
+            
+            dataset = SamplesDataset(dataset_tuples, columns_names=columns_names)
+            dataset.export_as_csv(nn_csv_data_path)
+            
     print(f'Dataset size : {len(dataset)}')
         
     return dataset
-    
